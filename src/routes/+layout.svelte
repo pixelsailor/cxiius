6208e3395a4d523ae5f1d9c1ca9989633dc1d759
pixelsailor <smoov.b@gmail.com>
2026-04-16 18:09:17 -0500

    add font switcher

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,39 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { Button, Popover, Slider } from 'bits-ui';
+
 	import { resolve } from '$app/paths';
 	import favicon from '$lib/assets/favicon.svg';
-	import { removeNoJsClassFromBody } from '$lib/utils/jsEnabled';
+	import { isJavaScriptEnabled, removeNoJsClassFromBody } from '$lib/utils/jsEnabled';
 	import type { RouteId } from '$app/types';
 
 	let { children } = $props();
 
-	onMount(() => {
-		removeNoJsClassFromBody();
-	});
+	let isJsEnabled = $state(isJavaScriptEnabled());
+
+	let serifFontFamily = 'Cambria, Cochin, Georgia, Times, Times New Roman, serif';
+	let sansFontFamily = 'Segoe UI, Helvetica Neue, Arial, sans-serif';
+
+	let activeFontFamily = $state(serifFontFamily);
+
+	let themeValue = $state(0);
 
 	const navItems: { label: string; path: RouteId }[] = [
 		{ label: 'Home', path: '/' },
 		{ label: 'Resume', path: '/resume' }
 	];
+
+	const toggleFontFamily = () => {
+		activeFontFamily = activeFontFamily === serifFontFamily ? sansFontFamily : serifFontFamily;
+	};
+
+	const handleThemeChange = (value: number) => {
+		console.log(value);
+	};
+
+	onMount(() => {
+		removeNoJsClassFromBody();
+	});
 </script>
 
 <svelte:head>
@@ -22,17 +41,18 @@
 	<link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great&amp;display=swap" rel="stylesheet" />
 	<link href="https://unpkg.com/@csstools/normalize.css" rel="stylesheet" />
 	<link rel="stylesheet" href="/styles/tokens.css" />
-	<link rel="stylesheet" href="/styles/typography.css" />
+	<link rel="stylesheet" href="/styles/buttons.css" />
 	<link rel="stylesheet" href="/styles/layout.css" />
+	<link rel="stylesheet" href="/styles/typography.css" />
 	<link rel="stylesheet" href="/styles/app.css" />
 </svelte:head>
 
-<div class="layout-container">
+<div class="layout-container" style:font-family={activeFontFamily}>
 	<header>
 		<div>
-			<div class="title-card-container">
-				<h1 class="display-large fredericka title-card">CXII</h1>
-			</div>
+			<!-- <div class="title-card-container">
+				<h1 class="headline-medium fredericka title-card">CXII</h1>
+			</div> -->
 			<nav class="main-nav">
 				<ul class="main-nav-list">
 					{#each navItems as item (item.path)}
@@ -43,7 +63,25 @@
 				</ul>
 			</nav>
 		</div>
-		<div class="header-controls"></div>
+		<div class="header-controls">
+			{#if isJsEnabled}
+				<div class="button-group">
+					<Popover.Root>
+						<Popover.Trigger class="button text" id="theme-picker-trigger">Theme</Popover.Trigger>
+						<Popover.Portal>
+							<Popover.Content class="popover-content">
+								<Slider.Root class="slider-root" id="theme-picker-slider" type="single" bind:value={themeValue} onValueCommit={handleThemeChange}>
+									<Slider.Range />
+									<Slider.Thumb index={0} />
+									<Slider.Tick index={0} />
+								</Slider.Root>
+							</Popover.Content>
+						</Popover.Portal>
+					</Popover.Root>
+					<Button.Root class="button text icon" id="font-toggle" onclick={toggleFontFamily}>A</Button.Root>
+				</div>
+			{/if}
+		</div>
 	</header>
 	<main>
 		{@render children()}
@@ -74,12 +112,11 @@
     height: 100vh;
   } */
 
-	.title-card {
-		/* font-size: 5rem; */
+	/* .title-card {
 		letter-spacing: 0.15em;
 		line-height: 1;
 		margin: 0;
-	}
+	} */
 
 	.main-nav-list {
 		display: flex;
@@ -99,5 +136,16 @@
 		align-items: center;
 		padding: 0.25rem;
 		color: var(--foreground-alt);
+	}
+
+	:global(.popover-content) {
+		padding: 0.5rem;
+		border: 1px solid var(--border);
+		border-radius: var(--radius-popover);
+		background-color: var(--background);
+		color: var(--foreground);
+		box-shadow: var(--shadow-popover);
+		z-index: 30;
+		max-width: 328px;
 	}
 </style>
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { Button } from 'bits-ui';
 	import type { PageProps } from './$types';
 
 	let { data }: PageProps = $props();
@@ -14,96 +15,102 @@
 	<meta name="author" content="Benjamin Thompson" />
 </svelte:head>
 
-<div class="container">
-	<section class="identity-section will-fade">
-		<h1 class="headline-large name">{data.identity.name}</h1>
-		<h2 class="headline-small role">{data.identity.role}</h2>
-		<ul class="supporting-links">
-			<li class="supporting-link">
-				<a href="https://www.linkedin.com/in/{data.identity.contact.linkedin}">{data.identity.contact.linkedin}</a>
-			</li>
-			<li class="supporting-link">
-				<a href="https://www.dribbble.com/{data.identity.contact.dribbble}">{data.identity.contact.dribbble}</a>
-			</li>
-		</ul>
-	</section>
-	<section class="summary-section will-fade">
-		<p class="title-large summary">
-			Staff Front-End Engineer with 20+ years of experience architecting enterprise-scale web applications and design systems. Builds component-driven platforms that enforce
-			accessibility, consistency, and usability by design while enabling AI-assisted development workflows. Proven leader in aligning cross-functional teams to deliver performant,
-			maintainable, and user-centered solutions.
-		</p>
-	</section>
-	<section class="differentiators-section will-fade">
-		<dl class="differentiators-list">
-			{#each data.identity.differentiators as differentiator (differentiator.headline)}
-				<div class="differentiator-item">
-					<dt class="title-medium">{differentiator.headline}</dt>
-					<dd class="body-large">{differentiator.detail}</dd>
-				</div>
-			{/each}
-		</dl>
-	</section>
-	<section class="experience-section will-fade">
-		<h3 class="headline-small">Experience</h3>
-		{#each data.experience as experience (experience.title)}
-			<div class="experience-item will-fade">
-				<h4 class="title-large experience-item__title">{experience.title}</h4>
-				<div class="experience-item__company-details">
-					<h5 class="title-medium experience-item__company">{experience.company}</h5>
-					<div class="experience-item__location-dates">
-						<span class="title-small experience-item__location">{experience.location}</span>
-						<span class="title-small experience-item__dates">{experience.startDate} to {experience.endDate}</span>
+<div class="container" id="page-top">
+	<article class="page-content">
+		<section class="identity-section will-fade">
+			<h1 class="headline-large name">{data.identity.name}</h1>
+			<h2 class="headline-small role">{data.identity.role}</h2>
+			<ul class="supporting-links">
+				<li class="supporting-link">
+					<a href="https://www.linkedin.com/in/{data.identity.contact.linkedin}">{data.identity.contact.linkedin}</a>
+				</li>
+				<li class="supporting-link">
+					<a href="https://www.dribbble.com/{data.identity.contact.dribbble}">{data.identity.contact.dribbble}</a>
+				</li>
+			</ul>
+		</section>
+		<section class="summary-section will-fade">
+			<p class="title-large summary">
+				Staff Front-End Engineer with 20+ years of experience architecting enterprise-scale web applications and design systems. Builds component-driven platforms that enforce
+				accessibility, consistency, and usability by design while enabling AI-assisted development workflows. Proven leader in aligning cross-functional teams to deliver
+				performant, maintainable, and user-centered solutions.
+			</p>
+		</section>
+		<section class="differentiators-section will-fade">
+			<dl class="differentiators-list">
+				{#each data.identity.differentiators as differentiator (differentiator.headline)}
+					<div class="differentiator-item">
+						<dt class="title-medium">{differentiator.headline}</dt>
+						<dd class="body-large">{differentiator.detail}</dd>
+					</div>
+				{/each}
+			</dl>
+		</section>
+		<section class="experience-section will-fade">
+			<h3 class="headline-small">Experience</h3>
+			{#each data.experience as experience (experience.title)}
+				<div class="experience-item will-fade">
+					<h4 class="title-large experience-item__title">{experience.title}</h4>
+					<div class="experience-item__company-details">
+						<h5 class="title-medium experience-item__company">{experience.company}</h5>
+						<div class="experience-item__location-dates">
+							<span class="title-small experience-item__location">{experience.location}</span>
+							<span class="title-small experience-item__dates">{experience.startDate} to {experience.endDate}</span>
+						</div>
+					</div>
+					<div class="experience-item__contributions">
+						<p class="body-large experience-item__context">{experience.context}</p>
+						<ul class="experience-item__contributions-list">
+							{#each experience.contributions as contribution (contribution)}
+								<li class="body-large experience-item__contribution">{contribution}</li>
+							{/each}
+						</ul>
 					</div>
 				</div>
-				<div class="experience-item__contributions">
-					<p class="body-large experience-item__context">{experience.context}</p>
-					<ul class="experience-item__contributions-list">
-						{#each experience.contributions as contribution (contribution)}
-							<li class="body-large experience-item__contribution">{contribution}</li>
-						{/each}
-					</ul>
-				</div>
-			</div>
-		{/each}
-	</section>
-	<section class="skills-section will-fade">
-		<h3 class="headline-small">Skills</h3>
-		<div class="skills-list">
-			{#each data.skills as skillCategory (skillCategory.name)}
-				<div class="skill-category">
-					<h4 class="title-small skill-category__name">{skillCategory.name}</h4>
-					<ul class="skill-category__skills-list">
-						{#each skillCategory.skills as skill (skill.name)}
-							<li class="skill-category__skill-item">{skill.name}</li>
-						{/each}
-					</ul>
-				</div>
 			{/each}
-		</div>
-	</section>
+		</section>
+		<section class="skills-section will-fade">
+			<h3 class="headline-small">Skills</h3>
+			<div class="skills-list">
+				{#each data.skills as skillCategory (skillCategory.name)}
+					<div class="skill-category">
+						<h4 class="title-small skill-category__name">{skillCategory.name}</h4>
+						<ul class="skill-category__skills-list">
+							{#each skillCategory.skills as skill (skill.name)}
+								<li class="skill-category__skill-item">{skill.name}</li>
+							{/each}
+						</ul>
+					</div>
+				{/each}
+			</div>
+		</section>
 
-	<section class="education-section will-fade">
-		<h3 class="headline-small education-section__title">Education</h3>
-		{#each data.education as education (education.credential)}
-			<div class="education-item will-fade">
-				<h4 class="title-medium education-item__title">{education.credential}</h4>
-				<div class="education-item__institution-details">
-					<h5 class="title-small education-item__institution">{education.institution}</h5>
-					<div class="education-item__location-dates">
-						<span class="title-small education-item__location">{education.location}</span>
-						<span class="title-small education-item__dates">{education.completedDate}</span>
+		<section class="education-section will-fade">
+			<h3 class="headline-small education-section__title">Education</h3>
+			{#each data.education as education (education.credential)}
+				<div class="education-item will-fade">
+					<h4 class="title-medium education-item__title">{education.credential}</h4>
+					<div class="education-item__institution-details">
+						<h5 class="title-small education-item__institution">{education.institution}</h5>
+						<div class="education-item__location-dates">
+							<span class="title-small education-item__location">{education.location}</span>
+							<span class="title-small education-item__dates">{education.completedDate}</span>
+						</div>
 					</div>
+					<ul class="education-item__honours-list">
+						{#each education.honours as honour (honour)}
+							<li class="body-large education-item__honour">{honour}</li>
+						{/each}
+					</ul>
+					<p class="body-medium education-item__notes">{education.notes}</p>
 				</div>
-				<ul class="education-item__honours-list">
-					{#each education.honours as honour (honour)}
-						<li class="body-large education-item__honour">{honour}</li>
-					{/each}
-				</ul>
-				<p class="body-medium education-item__notes">{education.notes}</p>
-			</div>
-		{/each}
-	</section>
+			{/each}
+		</section>
+	</article>
+
+	<div class="scroll-to-top-container">
+		<Button.Root href="#page-top" class="button" id="scroll-to-top-button">Scroll to Top</Button.Root>
+	</div>
 </div>
 
 <style>
@@ -116,20 +123,20 @@
 		list-style: none;
 	}
 
-	.container {
+	.page-content {
 		color: var(--foreground);
 	}
 
-	.container:hover .will-fade {
+	.page-content:hover .will-fade {
 		color: var(--muted);
 		text-shadow: 0 0 10px var(--muted);
 	}
 
-	.container .will-fade {
+	.page-content .will-fade {
 		transition: color 0.45s ease-in-out;
 	}
 
-	.container .will-fade:hover {
+	.page-content .will-fade:hover {
 		color: var(--foreground);
 		text-shadow: none;
 	}
