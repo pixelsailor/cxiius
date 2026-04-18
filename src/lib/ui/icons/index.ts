932ae7f402382b4d5c8cb8ed12de94a843aad8d5
pixelsailor <smoov.b@gmail.com>
2026-04-18 00:59:37 -0500

    export icons

--- src/lib/ui/icons/index.ts
+++ src/lib/ui/icons/index.ts
@@ -1,4 +1,8 @@
-import PaletteIcon from './PaletteIcon.svelte';
 import DesktopIcon from './DesktopIcon.svelte';
+import DimSunIcon from './DimSunIcon.svelte';
+import MoonIcon from './MoonIcon.svelte';
+import MoonStarsIcon from './MoonStarsIcon.svelte';
+import PaletteIcon from './PaletteIcon.svelte';
+import SunIcon from './SunIcon.svelte';
 
-export { PaletteIcon, DesktopIcon };
+export { PaletteIcon, DesktopIcon, MoonIcon, MoonStarsIcon, SunIcon, DimSunIcon };
