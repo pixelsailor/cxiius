4eb2528c0ca4a0e2376b861c82b05685a5889271
pixelsailor <smoov.b@gmail.com>
2026-04-18 00:57:33 -0500

    add theme icons

---
+++ src/lib/ui/icons/IconSize.ts
@@ -0,0 +1,9 @@
+export type IconSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+
+export const iconSizes: Record<IconSize, number> = {
+  xs: 16,
+  sm: 24,
+  md: 32,
+  lg: 40,
+  xl: 48
+};
---
+++ src/lib/ui/icons/index.ts
@@ -0,0 +1,4 @@
+import PaletteIcon from './PaletteIcon.svelte';
+import DesktopIcon from './DesktopIcon.svelte';
+
+export { PaletteIcon, DesktopIcon };
