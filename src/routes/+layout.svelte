3bf1e10f224ac997ce6fe612627d4f6634b62b93
pixelsailor <smoov.b@gmail.com>
2026-04-30 09:39:45 -0500

    fix fouc -- move linked styles to app head

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -95,17 +95,6 @@
 
 <svelte:head>
   <link rel="icon" href={favicon} />
-  <link href="https://fonts.googleapis.com/css?family=Fredericka+the+Great&amp;display=swap" rel="stylesheet" />
-  <link href="https://unpkg.com/@csstools/normalize.css" rel="stylesheet" />
-  <link rel="stylesheet" href="/styles/tokens.css" />
-  <link rel="stylesheet" href="/styles/buttons.css" />
-  <link rel="stylesheet" href="/styles/command.css" />
-  <link rel="stylesheet" href="/styles/dialogs.css" />
-  <link rel="stylesheet" href="/styles/forms.css" />
-  <link rel="stylesheet" href="/styles/layout.css" />
-  <link rel="stylesheet" href="/styles/markdown.css" />
-  <link rel="stylesheet" href="/styles/typography.css" />
-  <link rel="stylesheet" href="/styles/app.css" />
 </svelte:head>
 
 <div class={['layout-container', isLargeScreen.current ? '' : 'layout-container--small']} style:font-family={activeFontFamily}>
